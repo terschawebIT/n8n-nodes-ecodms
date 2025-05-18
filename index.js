2c57e09fa112dbb6747de71b7fa9250fce13e2f3
Niko Terschawetz <niko.terschawetz@digitalnativealliance.com>
2025-05-18 21:29:51 +0200

    feat: add index.js as entry point to fix loading issues

---
+++ index.js
@@ -0,0 +1,17 @@
+// Dieses Modul exportiert alle Nodes, die in diesem Paket enthalten sind
+// Es dient als Einstiegspunkt für n8n, um die Nodes zu laden
+
+module.exports = {
+    // Nodes im dist-Ordner, werden von n8n genutzt
+    // Diese Einträge müssen mit den Pfaden in package.json übereinstimmen
+    get nodeTypes() {
+        return [
+            require('./dist/nodes/EcoDMS/EcoDMS.node.js')
+        ];
+    },
+    get credentialTypes() {
+        return [
+            require('./dist/credentials/EcoDmsApi.credentials.js')
+        ];
+    }
+}; 
